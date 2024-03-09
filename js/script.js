4bb52cfdead85b4a69b478f13c43c9e3569ccc47
TrojanDll <60787064+TrojanDll@users.noreply.github.com>
2024-03-09 20:37:51 +0300

    feat: services, about adaptations added

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const sidebar = document.querySelector(".header__sidebar");
 const burger = document.querySelector(".header__burger");
 const close = document.querySelector(".header__sidebar__close");
-const sidebarLink = document.querySelector(".header__sidebar__list-item");
+const sidebarLinks = document.querySelectorAll(".header__sidebar__link");
 
 burger.addEventListener("click", () => {
   sidebar.classList.add("header__sidebar_opened");
@@ -11,6 +11,22 @@ close.addEventListener("click", () => {
   sidebar.classList.remove("header__sidebar_opened");
 });
 
-sidebarLink.addEventListener("click", () => {
-  sidebar.classList.remove("header__sidebar_opened");
+sidebarLinks.forEach((sidebarLink) => {
+  sidebarLink.addEventListener("click", (e) => {
+    sidebar.classList.remove("header__sidebar_opened");
+  });
 });
+
+// плавный скролл к якорю
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+  smoothLink.addEventListener("click", function (e) {
+    e.preventDefault();
+    const id = smoothLink.getAttribute("href");
+
+    document.querySelector(id).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  });
+}
